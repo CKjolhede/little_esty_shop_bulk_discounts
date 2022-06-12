cdba7abb53a5649013e94408b92974c51deaf761
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-12 00:07:56 -0600

    TEST for bulk show page having content: percent, threshold

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :feature do
-  
+  context 'when merchants visit bulk discount show page' do
+    before(:each) do
+      @merchant1 = Merchant.create!(name: 'I Care')
+      @discount1 = @merchant1.bulk_discounts.create!(percent: 10, threshold: 100)
+      @discount2 = @merchant1.bulk_discounts.create!(percent: 20, threshold: 300)
+      @discount3 = @merchant1.bulk_discounts.create!(percent: 30, threshold: 400)
+    
+      visit "/merchant/#{@merchant1.id}/bulk_discounts/#{@discount2.id}"
+    end
+
+    it 'display the bulk discounts percentage and threshold information' do
+    expect(page).to have_content("Percent discount: 20%")
+    expect(page).to have_content("Min purchase quantity required: 300")
+    end
+  end
 end
