9e8bb7a7fffb0dba5e9d71df3ac97799feabaaed
Chris Kjolhede <ckjolhede@gmail.com>
2022-06-14 21:06:16 -0600

    TEST #items_discouted on invoice model

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -43,4 +43,22 @@ RSpec.describe Invoice, type: :model do
       
     end
   end
+  describe 'class methods' do
+    it 'will gather all items that qualify for a discount' do
+      @merchant1 = Merchant.create!(name: 'I Care')
+      @item_1 = Item.create!(name: "Shampoo", description: "This washes your hair", unit_price: 10, merchant_id: @merchant1.id, status: 1)
+      @item_8 = Item.create!(name: "Butterfly Clip", description: "This holds up your hair but in a clip", unit_price: 5, merchant_id: @merchant1.id)
+      @item3 = Item.create!(name: "Rogaine", description: "2 liter bulk purchase", unit_price: 200, merchant_id: @merchant1.id)
+      @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Smith')
+      @invoice_1 = Invoice.create!(customer_id: @customer_1.id, status: 2, created_at: "2012-03-27 14:54:09")
+      @ii_1 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_1.id, quantity: 25, unit_price: 10, status: 2)
+      @ii_11 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item_8.id, quantity: 10, unit_price: 10, status: 1)
+      @ii_111 = InvoiceItem.create!(invoice_id: @invoice_1.id, item_id: @item3.id, quantity: 1, unit_price: 220, status: 1)
+      @discount1 = @merchant1.bulk_discounts.create!(percent: 20, threshold: 25)
+      @discount2 = @merchant1.bulk_discounts.create!(percent: 10, threshold: 10)
+
+      expect(@invoice.items_discounted).to match_array([@item_1, @item_8])
+      expect(@invoice.items_discoutned).to_not include(@item_3)
+    end
+  end
 end
